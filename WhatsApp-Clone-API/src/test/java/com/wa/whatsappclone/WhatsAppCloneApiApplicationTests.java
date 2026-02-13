d26881c14171ce85d4ac7900c97e0cabae963108
Hamada Reda <enghamadareda444@gmail.com>
2026-02-13 17:10:58 +0200

    Add configurations for postgresql and keycloak services

---
+++ WhatsApp-Clone-API/src/test/java/com/wa/whatsappclone/WhatsAppCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wa.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppCloneApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
